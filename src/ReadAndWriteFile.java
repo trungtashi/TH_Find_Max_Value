e88115eff526602e83c44819d15c4cfebab2748a
trungtashi99 <trungtashi@gmai.com>
2022-08-25 14:36:50 +0700

    Thực hành tìm giá trị lớn nhất và ghi ra file

---
+++ src/ReadAndWriteFile.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadAndWriteFile {
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<Integer> numbers = readAndWriteFile.readFile("C:\\Users\\DELL\\Desktop\\Module_02_code\\16_IO_TextFile\\TH_Find_Max_Value\\src\\numbers.txt");
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile("C:\\Users\\DELL\\Desktop\\Module_02_code\\16_IO_TextFile\\TH_Find_Max_Value\\src\\result.txt",maxValue);
+    }
+    public List<Integer> readFile(String filePath){
+        List<Integer> numbers = new ArrayList<>();
+        try{
+            File file = new File(filePath);
+
+            if(!file.exists()){
+                throw new FileNotFoundException();
+            }
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            while ((line = br.readLine())!= null){
+                numbers.add(Integer.parseInt(line));
+            }
+            br.close();
+        }
+        catch (Exception e){
+            System.err.println("File không tồn tại or nội dung có lỗi!");
+        }
+        return numbers;
+    }
+
+    public void writeFile(String filePath, int max){
+        try{
+            FileWriter writer = new FileWriter(filePath,true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write("Giá trị lớn nhất là: " + max);
+            bufferedWriter.close();
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static int findMax(List<Integer> numbers){
+        int max = numbers.get(0);
+        for (int i = 0; i < numbers.size(); i++) {
+            if(max < numbers.get(i)){
+                max = numbers.get(i);
+            }
+        }
+        return max;
+    }
+}
+
